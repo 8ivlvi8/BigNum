3f23ec846741cf7263c6432359d578d136ab6149
8ivlvi8 <letam2002pk@gmail.com>
2022-12-12 20:31:13 +0700

    pull

---
+++ BigNum/BigNum.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigNum
+{
+    internal class BigNum
+    {
+        private char sign;
+        private string num;
+        public BigNum(string s)
+        {
+            if (s[0] == '-')
+            {
+                num = s.Remove(0, 1);
+                sign = '-';
+            }
+            else
+            {
+                num = s;
+                sign = '+';
+            }
+        }
+        public char Sign { get => sign; set => sign = value; }
+        public string Num { get => num; set => num = value; }
+        public char Compare(BigNum B1, BigNum B2)
+        {
+            // l: B1 less than B2
+            // g: B1 greater than B2
+            // e: B1 equal to B2
+            if (B1.Sign == B2.Sign)
+            {
+                if (B1.Num == B2.Num)
+                    return 'e';
+                else if (B1.Num.Length < B2.Num.Length)
+                    return 'l';
+
+            }
+            else
+            {
+                if (B1.Sign == '+')
+                    return 'g';
+                else
+                    return 'l';
+            }
+            return 'a';
+        }
+    }
+}
--- BigNum/Form1.Designer.cs
+++ BigNum/Form1.Designer.cs
@@ -28,19 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxBN1 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // textBoxBN1
+            // 
+            this.textBoxBN1.Location = new System.Drawing.Point(97, 9);
+            this.textBoxBN1.Multiline = true;
+            this.textBoxBN1.Name = "textBoxBN1";
+            this.textBoxBN1.Size = new System.Drawing.Size(519, 45);
+            this.textBoxBN1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(97, 60);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(519, 45);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(97, 111);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(519, 45);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Số thứ nhất";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số thứ hai";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Kết quả";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(648, 301);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxBN1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox textBoxBN1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
